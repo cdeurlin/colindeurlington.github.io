f2fb88b4af6d5b3ae7091075e2e4669d7ec426d5
cdeurlin <c.deurlington@gmail.com>
2026-07-25 16:13:35 -0400

    Remove commented contact form code
    Removed commented-out contact form HTML from script.js.

--- script.js
+++ script.js
@@ -136,67 +136,6 @@ of time for questions, and the amount of time for group collaboration."</i> <b>[
 
 `
 
-// contact:`
-
-// <h1>Contact</h1>
-
-// <p>
-// If you'd like to discuss research, collaboration, or have a question,
-// please send me a message below.
-// </p>
-
-// <form id="contactForm">
-
-//     <label>Name</label>
-//     <br>
-//     <input
-//         type="text"
-//         name="name"
-//         cols="12"
-//         placeholder="Your name"
-//         required>
-
-//     <br>
-//     <label>Email</label>
-//     <br>
-//     <input
-//         type="email"
-//         name="email"
-//         cols="12"
-//         placeholder="you@example.com"
-//         required>
-        
-//     <br>
-//     <label>Subject</label>
-//     <br>
-//     <input
-//         type="subject"
-//         name="subject"
-//         cols="12"
-//         placeholder="Write your subject..."
-//         required>
-
-//     <br>
-//     <label>Message</label>
-//     <br>
-//     <textarea
-//         name="message"
-//         rows="8"
-//         cols="24"
-//         placeholder="Write your message..."
-//         required></textarea>
-
-//     <br>
-//     <button type="submit">
-//         Send Message
-//     </button>
-
-// </form>
-
-// <div id="successMessage"></div>
-
-// `
-
 }; 
 
 const content = document.getElementById("content");
